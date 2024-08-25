f88e6e6381532b943f4f4ef9cc954e6490f9410b
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-08-25 13:23:46 +0300

    ...

--- app/(routes)/search/page.tsx
+++ app/(routes)/search/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-const SearchPage = () => {
+const SearchResults = () => {
 	const searchParams = useSearchParams();
 	const query = searchParams.get("query");
 
-	return <div>
+	return (
+		<div>
 			<h1>
 				Search Results for: {query}
 			</h1>
 			{/* Buraya arama sonuçlarını göstermek için ilgili bileşenleri ekleyebilirsiniz */}
-		</div>;
+		</div>
+	);
+};
+
+const SearchPage = () => {
+	return (
+		<Suspense fallback={<div>Loading search results...</div>}>
+			<SearchResults />
+		</Suspense>
+	);
 };
 
 export default SearchPage;
