8dbd50ba5717b1c8a1e54840deb5da87722aea9c
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-08-25 20:55:52 +0300

    changes

--- app/(routes)/_components/Datatable/columns.tsx
+++ app/(routes)/_components/Datatable/columns.tsx
@@ -1,11 +1,10 @@
-"use client";
+// components/Datatable/columns.tsx
 
 import { ColumnDef } from "@tanstack/react-table";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type Payment = {
 	id: string;
+	product: string;
 	amount: number;
 	status: "pending" | "processing" | "success" | "failed";
 	email: string;
@@ -14,14 +13,43 @@ export type Payment = {
 export const columns: ColumnDef<Payment>[] = [
 	{
 		accessorKey: "status",
-		header: "Status"
+		header: "Status",
+		cell: ({ row }) =>
+			<div className="capitalize">
+				{row.getValue("status")}
+			</div>
+	},
+	{
+		accessorKey: "product",
+		header: "Product",
+		cell: ({ row }) =>
+			<div className="capitalize">
+				{row.getValue("product")}
+			</div>
 	},
 	{
 		accessorKey: "email",
-		header: "Email"
+		header: "Email",
+		cell: ({ row }) =>
+			<div className="lowercase">
+				{row.getValue("email")}
+			</div>
 	},
 	{
 		accessorKey: "amount",
-		header: "Amount"
+		header: () => <div className="text-right">Amount</div>,
+		cell: ({ row }) => {
+			const amount = parseFloat(row.getValue("amount"));
+			const formatted = new Intl.NumberFormat("en-US", {
+				style: "currency",
+				currency: "USD"
+			}).format(amount);
+
+			return (
+				<div className="text-right font-medium">
+					{formatted}
+				</div>
+			);
+		}
 	}
 ];
