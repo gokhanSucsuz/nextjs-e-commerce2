63048473ff5fd544761829e1d6fc67d641f813ed
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-08-26 09:52:41 +0300

    metadata added

---
+++ lib/SiteMetadata.ts
@@ -0,0 +1,15 @@
+export const siteMetadata = {
+	title: "Gökhan SUÇSUZ | FullStack Developer | Software Developer",
+	author: "Gökhan SUÇSUZ",
+	headerTitle: "Gökhan SUÇSUZ - FullStack Developer | Software Developer",
+	description: "Next.js, Tailwind CSS, React.js vs.",
+	language: "en-us",
+	theme: "system",
+	siteUrl: "https://github.com/gokhanSucsuz",
+	siteLogo: "/logo.png",
+	socialBanner: "/socialmedia.jpg",
+	email: "coolpisces22@gmail.com",
+	github: "https://github.com/gokhanSucsuz",
+	linkedin: "https://www.linkedin.com/in/gokhan-sucsuz-0912a4173/",
+	locale: "en-US"
+};
